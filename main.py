15afcecf9fa297d28f5cbf5ac670e69c059d3d74
Sergey Varlamov <msisdn.com@gmail.com>
2024-07-03 11:00:31 -0500

    Added recursion implementation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ numbers, so we get 0, 1, 1, 2, 3, 5, 8, 13, 21, ..."""
 
 # Create the 20 first Fibonacci numbers
 
+# 1. Implementation using a for loop
 previous_number = 0
 next_number = 1
 print(previous_number)
@@ -12,3 +13,24 @@ for x in range(18):
     print(new_number)
     previous_number = next_number
     next_number = new_number
+
+# 2. Implementation using recursion
+print(0)
+print(1)
+count = 2
+
+
+def fibonacci(previous_number, next_number):
+    global count
+    if count <= 19:
+        new_number = previous_number + next_number
+        print(new_number)
+        previous_number = next_number
+        next_number = new_number
+        count += 1
+        fibonacci(previous_number, next_number)
+    else:
+        return
+
+
+fibonacci(0, 1)
